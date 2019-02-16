9f04ae9a5145dc293fd84c88900d6d83506f4e3c
gglee89 <gglee89@gmail.com>
2019-02-16 01:15:17 -0600

    Component: Include TopNAvigation for RendererContent.

---
+++ src/components/ContentRenderer/TopNavigationMenu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './topNavigationMenu.css';
+
+const TopNavigationMenu = () => {
+  return (
+    <div className="content-renderer-top-navigation-container">
+      <div className="content-renderer-top-navigation-container-item">
+        General
+      </div>
+      <div className="content-renderer-top-navigation-container-item">
+        Screenshots
+      </div>
+      <div className="content-renderer-top-navigation-container-item">
+        About the Company
+      </div>
+    </div>
+  );
+};
+
+export default TopNavigationMenu;
--- src/components/ContentRenderer/index.js
+++ src/components/ContentRenderer/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './contentRenderer.css';
 
+// Components
+import TopNavigationMenu from './TopNavigationMenu';
+
 const ContentRenderer = () => {
-  return <div className="renderer-container" />;
+  return (
+    <div className="renderer-container">
+      <TopNavigationMenu />
+    </div>
+  );
 };
 
 export default ContentRenderer;
