38d9b2bceb4c2b2836de4b1f9e1ff12942a2febb
gglee89 <gglee89@gmail.com>
2019-02-15 21:13:02 -0600

    Page: Include NotFound page to handle non-existent routes.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './styles.default.css';
 import App from './App';
+import NotFound from './pages/NotFound';
 import * as serviceWorker from './serviceWorker';
 
 // Router
@@ -16,6 +17,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Route match path="/" component={App} />
+      <Route component={NotFound} />
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
