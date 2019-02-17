a9cdbedfd43b2875883deab89358d9cc94b73fa7
gglee89 <gglee89@gmail.com>
2019-02-17 00:47:45 -0600

    hotfix: Remove Into and Footer.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,8 @@
 import React, { PureComponent } from 'react';
 
 // Sections
-import Intro from '../../section/Intro';
 import Header from '../../section/Header';
 import Preferences from '../../section/Preferences';
-import Footer from '../../section/Footer';
 
 class Home extends PureComponent {
   render() {
