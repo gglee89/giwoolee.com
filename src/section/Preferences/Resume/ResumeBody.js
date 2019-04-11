a901e9e04d886a6c5f8897a99853056a5bbc7fae
gglee89 <gglee89@gmail.com>
2019-04-10 23:13:06 -0500

    Style: Include Education.

--- src/section/Preferences/Resume/ResumeBody.js
+++ src/section/Preferences/Resume/ResumeBody.js
@@ -13,24 +13,50 @@ const ResumeExperience = () => {
       <div className="ResumeBody__experience-box--date">11/2014 - Present</div>
       <ul className="ResumeBody__experience-box--achievements">
         <li>
-          <span>
-            Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
-            velit erat.
-          </span>
+          Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
+          velit erat.
         </li>
         <li>
-          <span>
-            Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
-            velit erat.
-          </span>
+          Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
+          velit erat.
         </li>
         <li>
-          <span>
-            Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
-            velit erat.
-          </span>
+          Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
+          velit erat.
         </li>
       </ul>
+    </div>
+  );
+};
+
+const ResumeEducation = () => {
+  return (
+    <div className="ResumeBody__education-item">
+      <div className="ResumeBody__education-item--title">
+        Ph.D. in Computer Science
+      </div>
+      <div className="ResumeBody__education-item--subtitle">
+        University of Mississippi
+      </div>
+      <div className="ResumeBody__education-item--date">
+        <div>08/2008 - 05/2010</div>
+        <div>Oxford, MS</div>
+      </div>
+      <div className="ResumeBody__education-item--list">
+        <div className="ResumeBody__education-item--list-title">
+          Selected Courses
+        </div>
+        <div className="ResumeBody__education-item--list-wrapper">
+          <ul>
+            <li>Computer-Aided Design</li>
+            <li>Dynamic Engineering Systems</li>
+          </ul>
+          <ul>
+            <li>Progamming for Engineers</li>
+            <li>Circuits</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
@@ -58,6 +84,48 @@ const ResumeBody = ({ color }) => {
       </div>
       <div className="ResumeBody__50">
         <div className="ResumeBody__content--title">SKILLS</div>
+        <div className="ResumeBody__skills">
+          <div className="ResumeBody__skills--item">
+            UI Engineer (Web/Native)
+          </div>
+          <div className="ResumeBody__skills--item">ReactJS</div>
+          <div className="ResumeBody__skills--item">NodeJS</div>
+          <div className="ResumeBody__skills--item">Machine Learning</div>
+          <div className="ResumeBody__skills--item">Python</div>
+          <div className="ResumeBody__skills--item">ML/NLP</div>
+        </div>
+        <div className="ResumeBody__content--title">TECHNICAL SKILLS</div>
+        <div className="ResumeBody__technical">
+          <div className="ResumeBody__technical-item">
+            <div className="ResumeBody__technical-item--title">Front End</div>
+            <div className="ResumeBody__technical-item--body">
+              Autodesk Revit, Building Information Modeling, Load Analysis
+              Programs, Energy Modeling Programs, AutoCad
+            </div>
+          </div>
+          <div className="ResumeBody__technical-item">
+            <div className="ResumeBody__technical-item--title">Back End</div>
+            <div className="ResumeBody__technical-item--body">
+              Autodesk Revit, Building Information Modeling, Load Analysis
+              Programs, Energy Modeling Programs, AutoCad
+            </div>
+          </div>
+          <div className="ResumeBody__technical-item">
+            <div className="ResumeBody__technical-item--title">
+              Machine Learning
+            </div>
+            <div className="ResumeBody__technical-item--body">
+              Autodesk Revit, Building Information Modeling, Load Analysis
+              Programs, Energy Modeling Programs, AutoCad
+            </div>
+          </div>
+        </div>
+        <div className="ResumeBody__content--title">EDUCATION</div>
+        <div className="ResumeBody__education">
+          <ResumeEducation />
+          <ResumeEducation />
+          <ResumeEducation />
+        </div>
       </div>
     </div>
   );
