62c3013985362aa89917128e66eb88edcae4fc27
gglee89 <gglee89@gmail.com>
2019-04-10 19:15:00 -0500

    Content: Add WorkExperience.

--- src/section/Preferences/Resume/ResumeBody.js
+++ src/section/Preferences/Resume/ResumeBody.js
@@ -3,8 +3,47 @@ import React from 'react';
 // Styles
 import './resumeBody.css';
 
+const ResumeExperience = () => {
+  return (
+    <div className="ResumeBody__experience-box">
+      <div className="ResumeBody__experience-box--title">
+        Mechanical/HVAC Engineer
+      </div>
+      <div className="ResumeBody__experience-box--company">TechUSA</div>
+      <div className="ResumeBody__experience-box--date">11/2014 - Present</div>
+      <ul className="ResumeBody__experience-box--achievements">
+        <li>
+          Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
+          velit erat.
+        </li>
+        <li>
+          Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
+          velit erat.
+        </li>
+        <li>
+          Praesent egestas elit est, eget convallis sem placerat quis. Ut eu
+          velit erat.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 const ResumeBody = () => {
-  return <div className="ResumeBody">ResumeBody</div>;
+  return (
+    <div className="ResumeBody">
+      <div className="ResumeBody__50">
+        <div className="ResumeBody__content--title">WORK EXPERIENCE</div>
+        <ResumeExperience />
+        <ResumeExperience />
+        <ResumeExperience />
+        <ResumeExperience />
+      </div>
+      <div className="ResumeBody__50">
+        <div className="ResumeBody__content--title">SKILLS</div>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeBody;
