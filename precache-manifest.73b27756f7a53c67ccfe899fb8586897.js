fc0bde12fe4602d219821c7454427199bfab0308
gglee89 <gglee89@gmail.com>
2019-11-24 20:28:25 -0500

    Updates

---
+++ precache-manifest.73b27756f7a53c67ccfe899fb8586897.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "b48bc876ed85918e210b0bc8cf17bbd2",
+    "url": "giwoolee.com/static/media/011-download.b48bc876.svg"
+  },
+  {
+    "revision": "66e6a611818d61616897",
+    "url": "giwoolee.com/static/css/main.008a5d09.chunk.css"
+  },
+  {
+    "revision": "0c7e6fe7bbfb53bb5858",
+    "url": "giwoolee.com/static/js/1.0c7e6fe7.chunk.js"
+  },
+  {
+    "revision": "22d1e76a756ef2ae9ec0",
+    "url": "giwoolee.com/static/js/runtime~main.22d1e76a.js"
+  },
+  {
+    "revision": "5bfa129d099dfbfffc5f8a56d231b9e8",
+    "url": "giwoolee.com/static/media/bg0.5bfa129d.jpg"
+  },
+  {
+    "revision": "318f80a14ff9b7c88e588745ebf95471",
+    "url": "giwoolee.com/static/media/bg1.318f80a1.jpg"
+  },
+  {
+    "revision": "bf356e5b74fde1bb5fe202da94c17938",
+    "url": "giwoolee.com/static/media/bg2.bf356e5b.jpg"
+  },
+  {
+    "revision": "d50d435e7df176fcac49ce7e065c7959",
+    "url": "giwoolee.com/static/media/bg3.d50d435e.jpg"
+  },
+  {
+    "revision": "806c565338f4036be1ae5aaa890e5128",
+    "url": "giwoolee.com/static/media/bg4.806c5653.jpg"
+  },
+  {
+    "revision": "c3305cae10085fc2ee67800823883ca5",
+    "url": "giwoolee.com/static/media/bg5.c3305cae.jpg"
+  },
+  {
+    "revision": "8c546a2faaaa16dd0a5c12c866a53dec",
+    "url": "giwoolee.com/static/media/bg6.8c546a2f.jpg"
+  },
+  {
+    "revision": "817fd63ce62b42f139815425a31d1afb",
+    "url": "giwoolee.com/static/media/apple.817fd63c.svg"
+  },
+  {
+    "revision": "208b2b42f87151f5b2f679cd1b761ada",
+    "url": "giwoolee.com/static/media/006-computer.208b2b42.svg"
+  },
+  {
+    "revision": "3b9d86ace5fc0f534400e2ba66c4e17d",
+    "url": "giwoolee.com/static/media/cookie.3b9d86ac.svg"
+  },
+  {
+    "revision": "f20258f89db68f55b8391c38aa09ec79",
+    "url": "giwoolee.com/static/media/ice-cream.f20258f8.svg"
+  },
+  {
+    "revision": "23e2bd8774519e2dd3176c6ebeb3dd2c",
+    "url": "giwoolee.com/static/media/melon.23e2bd87.svg"
+  },
+  {
+    "revision": "25e8ad73b4716e9950d3f1d2ac832937",
+    "url": "giwoolee.com/static/media/piece-of-cake.25e8ad73.svg"
+  },
+  {
+    "revision": "bc0d1d5f8ddced4a2782eee3f149420a",
+    "url": "giwoolee.com/static/media/sushi.bc0d1d5f.svg"
+  },
+  {
+    "revision": "7ccd1f68dddcb9c72c0e448105a9ce8e",
+    "url": "giwoolee.com/static/media/pizza.7ccd1f68.svg"
+  },
+  {
+    "revision": "c869b3d7b244000c57b8454aa6d614f7",
+    "url": "giwoolee.com/static/media/029-illustration.c869b3d7.svg"
+  },
+  {
+    "revision": "8e87866eb7ea54266bb71dd7f087b186",
+    "url": "giwoolee.com/static/media/046-like.8e87866e.svg"
+  },
+  {
+    "revision": "7bfdfea05041b2c364a4c80f0cd6aaf3",
+    "url": "giwoolee.com/static/media/015-explore.7bfdfea0.svg"
+  },
+  {
+    "revision": "81edbda8dd668b15c841149facec583f",
+    "url": "giwoolee.com/static/media/033-camera.81edbda8.svg"
+  },
+  {
+    "revision": "c613653cd3ad488786ea11bf4979353b",
+    "url": "giwoolee.com/static/media/027-contacts.c613653c.svg"
+  },
+  {
+    "revision": "f48b35510c0b67c4a4c4b79e4b89503f",
+    "url": "giwoolee.com/static/media/012-home.f48b3551.svg"
+  },
+  {
+    "revision": "d59408e0998e62e14b1622793ff1c9c0",
+    "url": "giwoolee.com/static/media/040-twitter.d59408e0.svg"
+  },
+  {
+    "revision": "66d1afe44a922a383130b20a6c642791",
+    "url": "giwoolee.com/static/media/044-email.66d1afe4.svg"
+  },
+  {
+    "revision": "66e6a611818d61616897",
+    "url": "giwoolee.com/static/js/main.66e6a611.chunk.js"
+  },
+  {
+    "revision": "adc9663d7e48d5df17963624b5814a42",
+    "url": "giwoolee.com/static/media/034-notebook.adc9663d.svg"
+  },
+  {
+    "revision": "f1bad1db814c17841be04d2d63e01102",
+    "url": "giwoolee.com/static/media/041-share.f1bad1db.svg"
+  },
+  {
+    "revision": "9320d8f4ff0d8c010bfd0c2fb4ecb8d7",
+    "url": "giwoolee.com/static/media/020-phone.9320d8f4.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "giwoolee.com/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "5bf8a3ca0bb6bfb41063cdd672be9ad9",
+    "url": "giwoolee.com/static/media/GraphQL_Logo.5bf8a3ca.svg"
+  },
+  {
+    "revision": "2b002415248676d3e0d101596dd70bcd",
+    "url": "giwoolee.com/static/media/apollo-client.2b002415.svg"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "giwoolee.com/static/media/Node.js_logo.89ccb090.svg"
+  },
+  {
+    "revision": "1a1e8e9707532072e3b314a98c19c31b",
+    "url": "giwoolee.com/static/media/freepik.1a1e8e97.svg"
+  },
+  {
+    "revision": "2e013d7501cea345c15e0ed7b6531464",
+    "url": "giwoolee.com/static/media/flaticon-logo.2e013d75.svg"
+  },
+  {
+    "revision": "259e1339a21cf9a16881f722296cb704",
+    "url": "giwoolee.com/static/media/Amazon_Web_Services_Logo.259e1339.svg"
+  },
+  {
+    "revision": "640981b3fb07858c389ca750c3849723",
+    "url": "giwoolee.com/static/media/mlab-vector-logo.640981b3.svg"
+  },
+  {
+    "revision": "09a3a4dce2329a14004a0e45f39c23f9",
+    "url": "giwoolee.com/static/media/043-view.09a3a4dc.svg"
+  },
+  {
+    "revision": "7906b11db00022ad02c3afe402e13ed2",
+    "url": "giwoolee.com/static/media/home-min.7906b11d.jpeg"
+  },
+  {
+    "revision": "486aaaaf06d1c7b82a7838765c3ca102",
+    "url": "giwoolee.com/static/media/habits-select-min.486aaaaf.jpeg"
+  },
+  {
+    "revision": "aa878ca080d6cabc9321ac485d528a9c",
+    "url": "giwoolee.com/static/media/dashboard-min.aa878ca0.jpeg"
+  },
+  {
+    "revision": "f60ca5cb506676ee977c35f67b9d2f64",
+    "url": "giwoolee.com/static/media/category-select-min.f60ca5cb.jpeg"
+  },
+  {
+    "revision": "69195f6e98e468494f99afde7778d51e",
+    "url": "giwoolee.com/static/media/proof-main-screen.69195f6e.png"
+  },
+  {
+    "revision": "5d6a1ea6a709c2d2002a414f854222ec",
+    "url": "giwoolee.com/static/media/proof-institutions-screen.5d6a1ea6.png"
+  },
+  {
+    "revision": "a3b6f607fe60ee3a16b4b7b8dc32ecca",
+    "url": "giwoolee.com/static/media/proof-main-screen-2.a3b6f607.jpg"
+  },
+  {
+    "revision": "5c37688dac520eb586e360c26784971a",
+    "url": "giwoolee.com/static/media/proof-multiple-charts.5c37688d.png"
+  },
+  {
+    "revision": "233e3fabab9db71c0e282af1f9d9a723",
+    "url": "giwoolee.com/static/media/sap-labs-hdbcsi.233e3fab.jpeg"
+  },
+  {
+    "revision": "728a03e72d91b28f2b5826ff32a3363b",
+    "url": "giwoolee.com/static/media/makestar-main.728a03e7.png"
+  },
+  {
+    "revision": "7ae5457bcff1da273c5bd7363e90d2c9",
+    "url": "giwoolee.com/static/media/makestar-project.7ae5457b.jpg"
+  },
+  {
+    "revision": "3a93f7261d6f604b2596cb3ba0acdbe8",
+    "url": "giwoolee.com/static/media/makestar-project-2.3a93f726.jpg"
+  },
+  {
+    "revision": "045a7c8a33faa09821ac500e9a65c23e",
+    "url": "giwoolee.com/static/media/marshall-goldsmith-min.045a7c8a.jpeg"
+  },
+  {
+    "revision": "c7b566fb5991ddc47a7e27730aa4b07f",
+    "url": "giwoolee.com/static/media/marshall-goldsmith-2-min.c7b566fb.jpeg"
+  },
+  {
+    "revision": "c4854d89540fd99d4925dbd83baa27e2",
+    "url": "giwoolee.com/index.html"
+  }
+];
